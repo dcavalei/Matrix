d292e51a11723da54710fad910c0216ed1c07236
Diogo Cavaleiro <ctw02079@criticaltechworks.com>
2022-03-30 17:34:04 +0100

    big upgrade WoW

--- inc/Matrix.hpp
+++ inc/Matrix.hpp
@@ -9,88 +9,36 @@ template <typename T>
 class Matrix
 {
 public:
-    Matrix() : Matrix(0, 0) {}
-
-    Matrix(size_t rows, size_t columns) : _rows(rows),
-                                          _columns(columns),
-                                          _size(rows * columns),
-                                          _buffer(std::make_unique<T[]>(rows * columns))
-    {
-        std::memset(_buffer.get(), 0, sizeof(T) * _size);
-    }
-
-    Matrix(Matrix const &other)
-    {
-        *this = other;
-    }
-
-    Matrix(Matrix &&other)
-    {
-        *this = std::move(other);
-    }
+    Matrix();
+    Matrix(size_t rows, size_t columns);
+    Matrix(Matrix const &other);
+    Matrix(Matrix &&other);
 
     /*--------------------------------------------------------------------------------------------*/
 
-    Matrix &operator=(Matrix const &other)
-    {
-        if (this == &other)
-        {
-            return *this;
-        }
-        this->~Matrix();
-        _rows = other._rows;
-        _columns = other._columns;
-        _buffer = std::make_unique<T[]>(other._rows * other._columns);
-        _size = other._size;
-        for (size_t i = 0; i < _size; i++)
-        {
-            _buffer[i] = other._buffer[i];
-        }
-        return *this;
-    }
-
-    Matrix &operator=(Matrix &&other)
-    {
-        if (this == &other)
-        {
-            return *this;
-        }
-
-        _buffer = other._buffer;
-        other._buffer = nullptr;
-        _rows = other._rows;
-        other._rows = 0;
-        _columns = other._columns;
-        other._columns = 0;
-        _size = other._size;
-        other._size = 0;
-        return *this;
-    }
-
-    T &operator()(size_t x, size_t y)
-    {
-        return _buffer[x * _columns + y];
-    }
-
-    T const &operator()(size_t x, size_t y) const
-    {
-        return _buffer[x * _columns + y];
-    }
+    Matrix operator+(Matrix const &other) const;
+    Matrix &operator+=(Matrix const &other);
+    Matrix operator-(Matrix const &other) const;
+    Matrix &operator-=(Matrix const &other);
+    Matrix operator*(T const &value) const;
+    Matrix &operator*=(T const &value);
+    Matrix operator*(Matrix const &other) const;
+    Matrix &operator*=(Matrix const &other);
+    Matrix &operator=(Matrix const &other);
+    Matrix &operator=(Matrix &&other);
+    T &operator()(size_t x, size_t y);
+    T const &operator()(size_t x, size_t y) const;
 
     /*--------------------------------------------------------------------------------------------*/
 
-    size_t size() const
-    {
-        return _size;
-    }
-
-    void print() const
-    {
-        for (size_t i = 0; i < _size; i++)
-        {
-            std::cout << _buffer[i] << (((i + 1) % _columns) ? '\t' : '\n');
-        }
-    }
+    size_t getRows() const;
+    size_t getColumns() const;
+    size_t size() const;
+    void print() const;
+    void random(T (*func)());
+    bool isEqualDim(Matrix const &other) const;
+    bool isMultipliableBy(Matrix const &other) const;
+    Matrix transpose() const;
 
 private:
     size_t _rows;
@@ -99,4 +47,259 @@ private:
     std::unique_ptr<T[]> _buffer;
 };
 
-// buffer = std::make_unique<T[]>( size );
+template <typename T>
+Matrix<T>::Matrix() : Matrix(0, 0) {}
+
+template <typename T>
+Matrix<T>::Matrix(size_t rows, size_t columns) : _rows(rows),
+                                                 _columns(columns),
+                                                 _size(rows * columns),
+                                                 _buffer(std::make_unique<T[]>(rows * columns))
+{
+    std::memset(_buffer.get(), 0, sizeof(T) * _size);
+}
+
+template <typename T>
+Matrix<T>::Matrix(Matrix const &other)
+{
+    *this = other;
+}
+
+template <typename T>
+Matrix<T>::Matrix(Matrix &&other)
+{
+    *this = std::move(other);
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator-(Matrix const &other) const
+{
+    if (isEqualDim(other) == false)
+    {
+        throw std::logic_error("Matrix dimension not equal!");
+    }
+
+    Matrix tmp(*this);
+
+    for (size_t i = 0; i < tmp._size; i++)
+    {
+        tmp._buffer[i] -= other._buffer[i];
+    }
+    return tmp;
+}
+
+template <typename T>
+Matrix<T> &Matrix<T>::operator-=(Matrix const &other)
+{
+    if (isEqualDim(other) == false)
+    {
+        throw std::logic_error("Matrix dimension not equal!");
+    }
+
+    for (size_t i = 0; i < _size; i++)
+    {
+        _buffer[i] -= other._buffer[i];
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(T const &value) const
+{
+    Matrix ret(*this);
+
+    for (size_t i = 0; i < _size; i++)
+    {
+        ret._buffer[i] *= value;
+    }
+    return (ret);
+}
+
+template <typename T>
+Matrix<T> &Matrix<T>::operator*=(T const &value)
+{
+    for (size_t i = 0; i < _size; i++)
+    {
+        _buffer[i] *= value;
+    }
+    return (*this);
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator*(Matrix const &other) const
+{
+    if (isMultipliableBy(other) == false)
+    {
+        throw std::logic_error("_columns != other._rows");
+    }
+
+    Matrix ret(_rows, other._columns); //  [n, p] = [n, m] . [m, p]
+
+    for (size_t i = 0; i < _rows; i++)
+    {
+        for (size_t j = 0; j < other._columns; j++)
+        {
+            for (size_t k = 0; k < other._rows; k++)
+            {
+                ret(i, j) += operator()(i, k) * other(k, j);
+            }
+        }
+    }
+    return (ret);
+}
+
+template <typename T>
+Matrix<T> &Matrix<T>::operator*=(Matrix const &other)
+{
+    if (isMultipliableBy(other) == false)
+    {
+        throw std::logic_error("_columns != other._rows");
+    }
+
+    return (*this = *this * other);
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::operator+(Matrix const &other) const
+{
+    if (isEqualDim(other) == false)
+    {
+        throw std::logic_error("Matrix dimension not equal!");
+    }
+
+    Matrix tmp(*this);
+
+    for (size_t i = 0; i < tmp._size; i++)
+    {
+        tmp._buffer[i] += other._buffer[i];
+    }
+    return tmp;
+}
+
+template <typename T>
+Matrix<T> &Matrix<T>::operator+=(Matrix const &other)
+{
+    if (isEqualDim(other) == false)
+    {
+        throw std::logic_error("Matrix dimension not equal!");
+    }
+
+    for (size_t i = 0; i < _size; i++)
+    {
+        _buffer[i] += other._buffer[i];
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T> &Matrix<T>::operator=(Matrix const &other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    _rows = other._rows;
+    _columns = other._columns;
+    _size = other._size;
+    _buffer = std::make_unique<T[]>(other._size);
+    for (size_t i = 0; i < _size; i++)
+    {
+        _buffer[i] = other._buffer[i];
+    }
+    return *this;
+}
+
+template <typename T>
+Matrix<T> &Matrix<T>::operator=(Matrix &&other)
+{
+    if (this == &other)
+    {
+        return *this;
+    }
+
+    _rows = other._rows;
+    other._rows = 0;
+    _columns = other._columns;
+    other._columns = 0;
+    _size = other._size;
+    other._size = 0;
+    _buffer = std::move(other._buffer);
+    return *this;
+}
+
+template <typename T>
+T &Matrix<T>::operator()(size_t x, size_t y)
+{
+    return _buffer[x * _columns + y];
+}
+
+template <typename T>
+T const &Matrix<T>::operator()(size_t x, size_t y) const
+{
+    return _buffer[x * _columns + y];
+}
+
+template <typename T>
+size_t Matrix<T>::getRows() const
+{
+    return _rows;
+}
+
+template <typename T>
+size_t Matrix<T>::getColumns() const
+{
+    return _columns;
+}
+
+template <typename T>
+size_t Matrix<T>::size() const
+{
+    return _size;
+}
+
+template <typename T>
+void Matrix<T>::print() const
+{
+    for (size_t i = 0; i < _size; i++)
+    {
+        std::cout << _buffer[i] << (((i + 1) % _columns) ? '\t' : '\n');
+    }
+}
+
+template <typename T>
+void Matrix<T>::random(T (*func)())
+{
+    for (size_t i = 0; i < _size; i++)
+    {
+        _buffer[i] = func();
+    }
+}
+
+template <typename T>
+bool Matrix<T>::isEqualDim(Matrix const &other) const
+{
+    return (_rows == other._rows && _columns == other._columns);
+}
+
+template <typename T>
+bool Matrix<T>::isMultipliableBy(Matrix const &other) const
+{
+    return (_columns == other._rows);
+}
+
+template <typename T>
+Matrix<T> Matrix<T>::transpose() const
+{
+    Matrix ret(_columns, _rows);
+
+    for (size_t x = 0; x < _rows; x++)
+    {
+        for (size_t y = 0; y < _columns; y++)
+        {
+            ret(y, x) = operator()(x, y);
+        }
+    }
+
+    return ret;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,75 @@
 #include "Matrix.hpp"
+#include <cstdlib>
 
 int main()
 {
-    Matrix<int> matrix;
-    Matrix<int> matrixXY(3, 6);
+    std::cout << std::boolalpha;
+    {
+        std::cout << "[*****************************************************]" << std::endl;
 
+        Matrix<int> A(6, 3);
+        Matrix<int> B(3, 6);
 
-    matrixXY(2, 4) = 10;
+        std::cout << "A(" << A.getRows() << ", " << A.getColumns() << ");" << std::endl;
+        std::cout << "B(" << B.getRows() << ", " << B.getColumns() << ");" << std::endl;
 
-    matrixXY.print();
+        std::cout << "\nAB - " << A.isMultipliableBy(B) << std::endl;
+        std::cout << "BA - " << B.isMultipliableBy(A) << std::endl;
+    }
+    {
+        std::cout << "[*****************************************************]" << std::endl;
 
-    matrix = matrixXY;
-    matrix.print();
+        Matrix<int> A(3, 5);
+        Matrix<int> B(5, 7);
 
+        std::cout << "A(" << A.getRows() << ", " << A.getColumns() << ");" << std::endl;
+        std::cout << "B(" << B.getRows() << ", " << B.getColumns() << ");" << std::endl;
+
+        std::cout << "\nAB - " << A.isMultipliableBy(B) << std::endl;
+        std::cout << "BA - " << B.isMultipliableBy(A) << std::endl;
+
+        srand(time(0));
+
+        auto func = []()
+        {
+            return (rand() % 10 + 1);
+        };
+
+        A.random(func);
+        B.random(func);
+        std::cout << "---------------------------------------------------------" << std::endl;
+        A.print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        B.print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        (A * B).print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        (A *= B).print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        A.print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        (A * 10).print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        A.print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        (A *= 10).print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        A.print();
+    }
+    {
+        std::cout << "---------------------------------------------------------" << std::endl;
+        Matrix<int> A(3, 6);
+
+        auto func = []()
+        {
+            return (rand() % 10);
+        };
+
+        A.random(func);
+        A.print();
+        std::cout << "---------------------------------------------------------" << std::endl;
+        A.transpose().print();
+    }
 
     return 0;
 }
